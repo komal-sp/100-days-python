21e1d441a468a648aee067e51f572605347794d4
ksphadtare5_54e4e3a07727 <ksphadtare5@gmail.com>
2023-11-30 20:34:13 +0530

    birthday wisher

--- monday motivation mail/monday_motivation.py
+++ monday motivation mail/monday_motivation.py
@@ -7,18 +7,23 @@ PASSWORD = "wyoc dsvz nmmx iypx"
 
 now = dt.datetime.now()
 weekday = now.weekday()
+print(weekday)
 
-if weekday == 2:
+if weekday == 3:
     with open("quotes.txt") as quote_file:
         all_quotes = quote_file.readlines()
-        quote1 = random.choice(all_quotes)
-        quote2 = random.choice(all_quotes)
+        quote = random.choice(all_quotes)
 
-    print(quote1)
+    print(quote)
     with smtplib.SMTP("smtp.gmail.com") as connection:
         connection.starttls()
         connection.login(MY_EMAIL, PASSWORD)
         # connection.sendmail(from_addr=MY_EMAIL, to_addrs="Papiyatiwari1999@gmail.com",
         #                     msg=f"Subject:Monday Motivation\n\n{quote1}")
-        connection.sendmail(from_addr=MY_EMAIL, to_addrs="papiyatiwari06051999@gmail.com",
-                            msg=f"Subject:Monday Motivation\n\n{quote2}")
+        # connection.sendmail(from_addr=MY_EMAIL, to_addrs="papiyatiwari06051999@gmail.com",
+        #                     msg=f"Subject:Monday Motivation\n\n{quote2}")
+        print(quote)
+        connection.sendmail(from_addr=MY_EMAIL, to_addrs="blossom.diaries29@gmail.com",
+                            msg=f"Subject:Monday Motivation\n\n{quote}")
+        connection.sendmail(from_addr=MY_EMAIL, to_addrs="vaibhavghadage22@gmail.com",
+                            msg=f"Subject:Monday Motivation\n\n{quote}")
